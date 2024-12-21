617950befb42101ec8f67dca2703248267cfd039
Gan Sheng Hong <shenghonggan@gmail.com>
2024-12-21 08:58:36 +0800

    fix: typo

--- pages/api/boards/[id]/move.ts
+++ pages/api/boards/[id]/move.ts
@@ -70,8 +70,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
       res.status(201).json({ message: "Board moved successfully" });
     } catch (error) {
-      console.log(error);
-      res.status(500).json({ error: "Error creating board" });
+      res.status(500).json({ message: "Error creating board", error });
     }
   }
 }
